df074dc635c1fc3c33146854ad58eaf0ff7be7ff
sagarsharma21 <007sharma.sagar@gmail.com>
2024-04-06 12:23:18 +0530

    Add Order-service

---
+++ backend/src/main/java/com/ecommerce/service/OrderService.java
@@ -0,0 +1,32 @@
+package com.ecommerce.service;
+
+import java.util.List;
+
+import com.ecommerce.exception.OrderException;
+import com.ecommerce.model.Address;
+import com.ecommerce.model.Order; //import com.ecommerce.model.Order  
+import com.ecommerce.model.User;
+
+public interface OrderService {
+	
+	public Order createOrder(User user, Address shippingAddress);
+	
+	public Order findOrderById(Long orderId) throws OrderException;
+	
+	public List<Order> usersOrderHistory(Long userId);
+	
+	public Order placedOrder(Long orderId) throws OrderException;
+	
+	public Order confirmedOrder(Long orderId)throws OrderException;
+	
+	public Order shippedOrder(Long orderId) throws OrderException;
+	
+	public Order deliveredOrder(Long orderId) throws OrderException;
+	 
+	public Order cancelledOrder(Long orderId) throws OrderException;
+	
+	public List<Order>getAllOrders();
+	
+	public void deleteOrder(Long orderId) throws OrderException;
+	
+}
