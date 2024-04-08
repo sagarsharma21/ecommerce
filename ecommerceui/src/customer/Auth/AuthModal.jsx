7b731249afaa9c6d38d3502a3abc7564124e89a5
Pardum Sharma <pardumsharma@Pardums-MacBook-Pro.local>
2024-04-08 13:07:06 +0530

    6 minor eror

--- ecommerceui/src/customer/Auth/AuthModal.jsx
+++ ecommerceui/src/customer/Auth/AuthModal.jsx
@@ -1,42 +1,43 @@
+
 import React from "react";
 import { Button, Box, Modal, Typography } from "@mui/material";
 import RegisterForm from "./RegisterForm";
+import LoginForm from "./LoginForm";
 import { useLocation } from "react-router-dom";
 
-  const style = {
-    position: 'absolute', //as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 500,
-    bgcolor: 'background.paper',
-    outline: 'none',
-    boxShadow: 24,
-    p: 4,
-  };
-
+ const style = {
+ position: 'absolute', //as 'absolute',
+ top: '50%',
+ left: '50%',
+ transform: 'translate(-50%, -50%)',
+ width: 500,
+ bgcolor: 'background.paper',
+ outline: 'none',
+  boxShadow: 24,
+ p: 4,
+ };
 
 const AuthModal = ({handleClose, handleOpen}) => {
-  const location=useLocation();
-  return (
-    <div>
-      AuthModal
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          {location.pathname==="/login"?<LoginForm/> :}
-          <RegisterForm/>
-          
-          <RegisterForm/>
-        </Box>
-      </Modal>
-    </div>
-  );
+ const location=useLocation();
+ return (
+ <div>
+ AuthModal
+ <Button onClick={handleOpen}>Open modal</Button>
+ <Modal
+ open={true}
+ onClose={handleClose}
+ aria-labelledby="modal-modal-title"
+ aria-describedby="modal-modal-description"
+ >
+ <Box sx={style}>
+ {location.pathname === "/login" ? ( <LoginForm/>) : (<RegisterForm/>) }
+ {/* <RegisterForm/>
+ <RegisterForm/> */}
+ </Box>
+ </Modal>
+ </div>
+ );
 };
 
 export default AuthModal;
+
